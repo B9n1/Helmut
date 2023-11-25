3f22769b0fdefe2603837b61126552a3b0072edb
B9n1 <benjamin.erkel@onlinehome.de>
2023-11-25 16:42:25 +0100

    Init

---
+++ frontend/src/app/component/content.tsx
@@ -0,0 +1,7 @@
+export default function Content() {
+  return (
+    <div className="Content CenterChild">
+      <h1>Welcome To Helmut</h1>
+    </div>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Content from "./component/content";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <div className="Menu"></div>
+      <div className="Content CenterContainer">
+        <Content />
+      </div>
+    </main>
+  );
+}
